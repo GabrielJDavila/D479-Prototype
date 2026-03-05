c2bdc65db4002aa459fb5ec3842bdf8ff6c30c40
Gabriel Davila <gdavila0819@gmail.com>
2026-03-04 21:07:32 -0600

    Added more css styles, basic JS functionality for mobile nav menu

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // js file
+const menuBtn = document.getElementById("menu-btn")
+const navMenu = document.getElementById("nav-menu")
+const closeMenuBtn = document.getElementById("close-menu-btn")
+
+menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("hidden")
+    menuBtn.classList.add("hidden")
+    closeMenuBtn.classList.toggle("hidden")
+})
+
+closeMenuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("hidden")
+    menuBtn.classList.remove("hidden")
+    closeMenuBtn.classList.toggle("hidden")
+})
